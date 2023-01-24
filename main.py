5ffbf45802b87ae9cea7bd2aea9683eb6f42b7d3
BLIPBLOP11 <77438952+BLIPBLOP11@users.noreply.github.com>
2023-01-24 19:41:59 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+import sys
+
+pygame.init()
+
+window_width, window_height = 1280, 720
+screen = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption('Game')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    pygame.display.update()
